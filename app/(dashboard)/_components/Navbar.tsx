cd953c7e40b6fcf1b06b08bcc58451417f473022
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-04-05 07:31:18 +0545

    "Add Navbar and import @radix-ui/react-dialog in dashboard layout".

---
+++ app/(dashboard)/_components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { NavbarRoutes } from "@/components/Navbar-Routes";
+import { MobileSidebar } from "./MobileSidebar";
+
+export const Navbar = () => {
+  return (
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <MobileSidebar />
+      <NavbarRoutes />
+    </div>
+  );
+};
