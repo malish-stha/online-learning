2b1a7eefc93728c92265963f848e3ae6a4008d47
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-04-05 15:31:20 +0545

    "Add conditional link to teacher mode or exit button in NavbarRoutes"

--- components/Navbar-Routes.tsx
+++ components/Navbar-Routes.tsx
@@ -1,11 +1,34 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 export const NavbarRoutes = () => {
+  const pathname = usePathname();
+
+  const isTeacherpage = pathname?.startsWith("/teacher");
+  const isPlayePage = pathname.includes("/chapter");
   return (
     <div className="flex gap-x-2 ml-auto">
-      <UserButton />
+      {isTeacherpage || isPlayePage ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
