0d082fc7bf6ef2cbc92bdf42f379112ac25fd50a
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-04-07 07:15:18 +0545

    "Add SearchInput to NavbarRoutes and conditionally render based on pathname; update layout of navigation buttons".

--- components/Navbar-Routes.tsx
+++ components/Navbar-Routes.tsx
@@ -6,29 +6,39 @@ import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
+import { SearchInput } from "./SearchInput";
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacherpage = pathname?.startsWith("/teacher");
   const isPlayePage = pathname.includes("/chapter");
+  const isSearchPage = pathname === "/search";
+
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherpage || isPlayePage ? (
-        <Link href="/">
-          <Button size="sm" variant="ghost">
-            <LogOut className="h-4 w-4 mr-2" />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button size="sm" variant="ghost">
-            Teacher Mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-      <UserButton afterSignOutUrl="/" />
-    </div>
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherpage || isPlayePage ? (
+          <Link href="/">
+            <Button size="sm" variant="ghost">
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Teacher Mode
+            </Button>
+          </Link>
+        )}
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </>
   );
 };
