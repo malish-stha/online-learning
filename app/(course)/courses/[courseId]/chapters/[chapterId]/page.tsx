abab739956dd6e716497d0b056cf30db1998ec0a
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-04-07 13:42:46 +0545

    "Add Separator component from Radix UI and display attachments in Chapter page".

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -4,6 +4,9 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { VideoPlayer } from "./_components/VideoPlayer";
 import { CourseEnrollButton } from "./_components/CourseEnrollButton";
+import { Separator } from "@/components/ui/separator";
+import { Preview } from "@/components/Preview";
+import { File } from "lucide-react";
 
 const ChapterIdPage = async ({
   params,
@@ -72,6 +75,28 @@ const ChapterIdPage = async ({
               />
             )}
           </div>
+          <Separator />
+          <div>
+            <Preview value={chapter.description!} />
+          </div>
+          {!!attachments.length && (
+            <>
+              <Separator />
+              <div className="p-4">
+                {attachments.map((attachment) => (
+                  <a
+                    href={attachment.url}
+                    target="_blank"
+                    key={attachment.id}
+                    className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
+                  >
+                    <File />
+                    <p className="line-clamp-1">{attachment.name}</p>
+                  </a>
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
