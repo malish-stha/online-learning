0f01201ac6154e64f0b63ea865cd899059e00d30
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-04-07 11:07:35 +0545

    "Updated SearchPage layout to add spacing between components".

---
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -0,0 +1,58 @@
+import { getProgress } from "@/actions/get-progress";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { CourseSidebar } from "./_components/CourseSidebar";
+
+const CourseLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { courseId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        include: {
+          userProgress: {
+            where: {
+              userId,
+            },
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+  const progressCount = await getProgress(userId, course.id);
+
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </div>
+      <main className="md:pl-80 h-full"> {children}</main>
+    </div>
+  );
+};
+
+export default CourseLayout;
