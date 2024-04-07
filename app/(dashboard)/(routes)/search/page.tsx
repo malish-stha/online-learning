51727a6184f0919a539287cc4005aad8bb4ffe78
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-04-07 10:02:59 +0545

    "Remove GetProgress action and update search page to fetch courses with user progress on server side".

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,13 +1,25 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/Categories";
 import { SearchInput } from "@/components/SearchInput";
+import { getCourses } from "@/actions/get-courses";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 const SearchPage = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
   });
+
+  const courses = await getCourses({
+    userId,
+  });
+
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
